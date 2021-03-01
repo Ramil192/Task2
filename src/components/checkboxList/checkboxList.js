eaa83a94e4e4f1b461ed5788981947d45d5ba0a8
John Doe <randomm200@mail.ru>
2021-03-01 22:47:30 +0300

    add com likeBtn

--- src/components/checkboxList/checkboxList.js
+++ src/components/checkboxList/checkboxList.js
@@ -1,7 +1,6 @@
 $(function(){
   const checkboxBtn = document.querySelector('#checkboxListBtn')
-  const checkboxItems = document.querySelector('.checkboxList__items')
-  console.log(checkboxBtn); 
+  const checkboxItems = document.querySelector('.checkboxList__items'); 
   checkboxBtn.addEventListener('click',()=>{
     checkboxItems.classList.toggle('checkboxList__items_show')
     checkboxBtn.classList.toggle('checkboxList__titleBtn_rotate')
---
+++ src/components/likeButton/likeButton.js
@@ -0,0 +1,13 @@
+$(function () {
+  const likeBtn = document.querySelector('.likeButton')
+  const countLick = likeBtn.firstElementChild
+  likeBtn.addEventListener('click', () => {
+    if (likeBtn.classList.contains('likeButton_action')) {
+      countLick.innerHTML--;
+      likeBtn.classList.remove('likeButton_action')
+    } else {
+      countLick.innerHTML++;
+      likeBtn.classList.add('likeButton_action')
+    }
+  })
+})
