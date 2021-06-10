1bda5b3c291a51680a9ad42d227e7456093202d9
John Doe <randomm200@mail.ru>
2021-06-10 10:46:27 +0300

    fix layout

--- src/pages/ui-kit/cards/cards.js
+++ src/pages/ui-kit/cards/cards.js
@@ -1,6 +1,8 @@
 import '../../../fonts/fonts.scss';
 import Slider from '../../../components/slide/slide'
+import Calendar from '../../../components/calendar/calendar';
 import './cards.scss';
 
 document.querySelectorAll('.rev').forEach(element => new Slider(element));
+document.querySelectorAll('.js-calendar').forEach(element => new Calendar(element));
 
